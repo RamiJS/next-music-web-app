09ecdaa221ccbe1d5d3fd5d521c01fb710b4f61c
TechRami <techrami@outlook.com>
2023-06-23 09:01:56 +0300

    created music player and controls

--- app/(site)/components/PageContent.tsx
+++ app/(site)/components/PageContent.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import SongItem from "@/components/SongItem";
+import useOnPlay from "@/hooks/useOnPlay";
 import { Song } from "@/utilities/types";
 
 interface Props {
@@ -8,6 +9,7 @@ interface Props {
 }
 
 const PageContent = ({ songs }: Props) => {
+  const onPlay = useOnPlay(songs);
   if (songs.length < 0) {
     return <div className="mt-4 text-neutral-400">No songs available</div>;
   }
